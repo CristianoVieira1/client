1528e999ca64f3856319e6de3f5d1c78a8fa3b63
CristianoVieira1 <cristianovieirati@gmail.com>
2022-04-04 21:35:07 -0300

    Configuracao de font e styles global

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,13 +1,13 @@
 import * as S from './styles'
 
 const Main = ({
-  title = 'React Avançado',
+  title = 'Boilerplate',
   description = 'TypeScript, ReactJS, NextJS e Styled Components'
 }) => (
   <S.Wrapper>
     <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um átomo e React Avançado escrito ao lado."
+      src="/img/logo-2.png"
+      alt="Imagem de um átomo e Boilerplate escrito ao lado."
     />
     <S.Title>{title}</S.Title>
     <S.Description>{description}</S.Description>
