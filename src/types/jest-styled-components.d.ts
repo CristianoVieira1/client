cf9c13db62406306b2e1ed71a222eadd395dcb25
CristianoVieira1 <cristianovieirati@gmail.com>
2022-04-15 16:26:16 -0300

    atualizando heading

--- src/types/jest-styled-components.d.ts
+++ src/types/jest-styled-components.d.ts
@@ -1,8 +1,5 @@
-// Types provided from the official repo:
-// https://github.com/styled-components/jest-styled-components/blob/master/typings/index.d.ts
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Plugin, NewPlugin } from 'pretty-format'
 
 declare global {
@@ -20,7 +17,6 @@ declare global {
       supports?: string
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interface Matchers<R, T> {
       toHaveStyleRule(property: string, value?: Value, options?: Options): R
     }
